aa1306633626f8d4b9a39b754d27e4b6e240315e
Thanos Farmer <73735520+NaufilAsar@users.noreply.github.com>
2022-07-11 23:26:21 +0530

    Update code.java

--- code.java
+++ code.java
@@ -36,7 +36,80 @@ public class Main // a class which is public must be in a file such that filenam
         // we generally name constant variables in Uppercase
         final SCREEN_SIZE = 566;
         
-	}
+        // Array
+        int[] arr = new int[3]; // make an empty array
+        int[] arr2 = { 1, 2, 3, 4, 5, 6} // make an array with values
+        
+        arr[2] = 4; // change the third element to value of 4
+        System.out.println(arr[2]); // print third element
+        
+// Control Flow Structures:
+    // Alter the default order/ sequence of code execution.. (i.e from line number 1 to end)
+    
+    // 1. Selection 
+    // if-else statements
+       if (3 < 4) { // supply a condition which retuens a boolean here
+           System.out.println("3 is less than 4."); // this is executed only if condition is true
+       }
+    // switch case // similar to multiple switch statements
+        char letter = 'C';
+        switch(number){
+            case 'A':
+                System.out.println("The letter is A");
+                break; // if this case is true then exit the switch statement, or else it will execute every case..
+            case 'B':
+                System.out.println("The letter is B");
+                break;
+            case 'C':
+                System.out.println("The letter is C");
+                break;
+            case default:
+                System.out.println("Wrong letter"); // default case is executed if all above cases are Wrong
+        }
+
+    // 2. Iteration / Looping
+    // while
+    int number = 10;
+    while(number != 0) { // execute all statements within brackets if number is not equal to 0 repeatedly.
+        System.out.println(number);
+        number++; // increase value of number by 1.
+    }
+    // for
+    for (int i = 0; i <= 10 ; i++ ) { // create a new int 'i'. loop until i is less than 10. increase value of i by 1 after every loop.
+        System.out.println(i);
+    }
+    // do-while // just a while loop which executes the loop statements without checking the condition for the very first loop.
+    // it executes at least once.
+    number = 10;
+    do {
+        System.out.println(number);
+        number++;
+    } while(number != 0);
+    // for-each // simplified for loop 
+    int [] intArray = { 10, 20, 30, 40, 50 };
+       
+    for( int value : intArray ) {
+       System.out.println( value );
+    }
+    
+    
+    // 3. Jump
+    // break // stop a loop
+    for (int i = 0; i <= 10 ; i++ ) { 
+        System.out.println(i);
+        if (i == 5)
+            break;
+    }
+    // continue // skip an iteration of a loop
+        for (int i = 0; i <= 10 ; i++ ) { 
+        System.out.println(i);
+        if (i == 5)
+            continue;
+    }
+    // return // return a value or no value at all and exit from a method
+    public int sum(int a, int b){
+        return a+b;
+    }
 }
 
 // a class consists of data in two types:
@@ -71,5 +144,3 @@ class Student extends Person {
     int roll_no;
     
 } 
-
-
